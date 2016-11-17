fdad1369a1dd6d282b6783d90dd6c360df6c1ca3
SajadAzami <sajjadaazami@gmail.com>
2016-11-17 15:32:07 +0330

    prediction improved

--- DMC2016/DMC16/model/prediction.py
+++ DMC2016/DMC16/model/prediction.py
@@ -11,6 +11,10 @@ __author__ = 'sajjadaazami@gmail.com (Sajad Azami)'
 train_df = pd.read_csv('../data_set/orders_train.txt', sep=';', header=0)
 test_df = pd.read_csv('../data_set/orders_class.txt', sep=';', header=0)
 
+# Use this if you want to train and test on train data
+# train_df = train_df[680001:860000]
+# test_df = train_df[2000001:2200000]
+
 
 class DataFrameImputer(TransformerMixin):
     def fit(self, X, y=None):

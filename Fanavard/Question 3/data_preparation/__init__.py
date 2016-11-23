5c390730acc8bd67d530f7af9328982feee21495
SajadAzami <sajjadaazami@gmail.com>
2016-11-23 16:27:34 +0330

    Fanavard question3 was added

---
+++ Fanavard/Question 3/data_preparation/__init__.py
@@ -0,0 +1,3 @@
+"""Question 3, 11/23/16, Sajad Azami"""
+
+__author__ = 'sajjadaazami@gmail.com (Sajad Azami)'
---
+++ Fanavard/Question 3/data_preparation/preprocessing.py
@@ -0,0 +1,7 @@
+"""Question 3, 11/23/16, Sajad Azami"""
+
+import pandas as pd
+
+__author__ = 'sajjadaazami@gmail.com (Sajad Azami)'
+
+pd.read_csv('../data_set/data_train.csv')

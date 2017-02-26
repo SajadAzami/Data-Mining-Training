8ffc327527665cd82b92c49a21f03b3ff11dfcfb
SajadAzami <sajjadaazami@gmail.com>
2017-02-27 01:05:46 +0330

    preprocessing almost finished, logistic regression done

--- DM17-AUT/HW1-Kaggle Titanic/Kaggle_Titanic/main.py
+++ DM17-AUT/HW1-Kaggle Titanic/Kaggle_Titanic/main.py
@@ -1,21 +1,15 @@
 """Kaggle_Titanic, 2/22/17, Sajad Azami"""
 
-from preprocessing import data_preparation as dp
-import pandas as pd
-import numpy as np
-import seaborn as sns
-from visualization import scatter
 import matplotlib.pyplot as plt
-from sklearn.pipeline import make_pipeline
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.feature_selection import SelectKBest
+import pandas as pd
+import seaborn as sns
 from sklearn.cross_validation import StratifiedKFold
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.grid_search import GridSearchCV
-from sklearn.ensemble.gradient_boosting import GradientBoostingClassifier
-from sklearn.cross_validation import cross_val_score
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.feature_selection import SelectFromModel
 from sklearn.linear_model import LogisticRegression
+
+from preprocessing import data_preparation as dp
+from visualization import scatter
 
 sns.set_style('white')
 __author__ = 'sajjadaazami@gmail.com (Sajad Azami)'
@@ -56,21 +50,21 @@ test_data_set['Embarked'] = test_data_set['Embarked'].astype(int)
 train_data_set['Sex'] = train_data_set['Sex'].astype(int)
 test_data_set['Sex'] = test_data_set['Sex'].astype(int)
 
-# # Scatter plot numerical data
-# scatter.bar_plot_feature_vs_label(train_data_set, 'Survived',
-#                                   ['Pclass', 'SibSp', 'Parch'], 2, 2)
+# Scatter plot numerical data
+scatter.bar_plot_feature_vs_label(train_data_set, 'Survived',
+                                  ['Pclass', 'SibSp', 'Parch'], 2, 2)
 
 # Drop unnecessary columns, these columns won't be useful in analysis and prediction
 train_data_set = train_data_set.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis=1)
 test_data_set_ids = test_data_set['PassengerId']
 test_data_set = test_data_set.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis=1)
 
-# # Peaks for survived/not survived passengers by their age
-# facet = sns.FacetGrid(train_data_set, hue="Survived", aspect=4)
-# facet.map(sns.kdeplot, 'Age', shade=True)
-# facet.set(xlim=(0, train_data_set['Age'].max()))
-# facet.add_legend()
-# plt.show()
+# Peaks for survived/not survived passengers by their age
+facet = sns.FacetGrid(train_data_set, hue="Survived", aspect=4)
+facet.map(sns.kdeplot, 'Age', shade=True)
+facet.set(xlim=(0, train_data_set['Age'].max()))
+facet.add_legend()
+plt.show()
 
 # Split train and test
 train_Y = train_data_set['Survived'].ix[0:690]
@@ -82,12 +76,12 @@ test_X = train_data_set.drop('Survived', axis=1).ix[691:]
 print(train_X.info())
 print(test_X.info())
 
-# # Logistic Regression model
-# log_reg = LogisticRegression()
-# log_reg.fit(train_X, train_Y)
-# Y_pred = log_reg.predict(test_data_set)
-# submission = pd.DataFrame(pd.concat([test_data_set_ids, pd.DataFrame(Y_pred)], axis=1))
-# submission.to_csv(path_or_buf='logistic_regression_result.csv', index=False)
+# Logistic Regression model
+log_reg = LogisticRegression()
+log_reg.fit(train_X, train_Y)
+Y_pred = log_reg.predict(test_data_set)
+submission = pd.DataFrame(pd.concat([test_data_set_ids, pd.DataFrame(Y_pred)], axis=1))
+submission.to_csv(path_or_buf='logistic_regression_result.csv', index=False)
 
 # Random Forest model
 forest = RandomForestClassifier(max_features='sqrt')
@@ -101,8 +95,9 @@ cross_validation = StratifiedKFold(train_Y, n_folds=5)
 grid_search = GridSearchCV(forest,
                            param_grid=parameter_grid,
                            cv=cross_validation)
-
 grid_search.fit(train_X, train_Y)
-
 print('Best score: {}'.format(grid_search.best_score_))
 print('Best parameters: {}'.format(grid_search.best_params_))
+Y_pred = grid_search.predict(test_data_set).astype(int)
+submission = pd.DataFrame(pd.concat([test_data_set_ids, pd.DataFrame(Y_pred)], axis=1))
+submission.to_csv(path_or_buf='random_forest.csv', index=False)
--- DM17-AUT/HW1-Kaggle Titanic/Kaggle_Titanic/submission_generator.py
+++
@@ -1,3 +0,0 @@
-"""Kaggle_Titanic, 2/22/17, Sajad Azami"""
-
-__author__ = 'sajjadaazami@gmail.com (Sajad Azami)'

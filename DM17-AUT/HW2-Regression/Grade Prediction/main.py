443715c7b792ffc28af4d421f5cf7533f8ac7e65
SajadAzami <sajjadaazami@gmail.com>
2017-03-05 02:06:34 +0330

    imputation using gausian mixture noise

--- DM17-AUT/HW2-Regression/Grade Prediction/main.py
+++ DM17-AUT/HW2-Regression/Grade Prediction/main.py
@@ -8,12 +8,16 @@ from matplotlib import gridspec
 import matplotlib.pyplot as plt
 from sklearn.linear_model import Lasso
 import pandas as pd
+import math
 
 __author__ = 'sajjadaazami@gmail.com (Sajad Azami)'
 sns.set_style("white")
 
 train_full_X, train_full_Y = data_preparation.read_data('./data_set/train.csv', 6)
 print('Data set Loaded!')
+
+# In case any non-zero NA
+train_full_X = train_full_X.fillna(0)
 
 # Split train and test data
 train_X = train_full_X[:200]
@@ -36,17 +40,31 @@ for i in range(0, 2):
         ax_temp.title.set_text(('Feature ' + str(counter)))
 plt.show()
 
-# Filling missing values with Gaussian Noise, N(mean_of_row, 1)
+# Filling missing values with Gaussian Noise, 0.5 * N(mean_of_row, 1) + 0.5 * N(mean_of_class, 1)
 for i in range(0, train_X.shape[0]):
     row = train_X.values[i]
     for j in range(0, len(row)):
         if row[j] == 0:
-            row[j] = abs(np.random.normal(sum(row) / len(row), scale=1))
-# Filling missing values of test data with the same way
+            # Considering only non-zero values in mean
+            std_mean = sum(row) / len(row[np.nonzero(row)])
+            if math.isnan(std_mean):
+                std_mean = np.random.normal(10, scale=1)
+            class_mean = sum(train_X[j]) / len(train_X[j].values[np.nonzero(train_X[j].values)])
+            row[j] = round(0.5 * (abs(np.random.normal(std_mean, scale=1))) +
+                           0.5 * (abs(np.random.normal(class_mean, scale=1))), 1)
+
+# Filling missing values for test data using same way
 for i in range(0, test_X.shape[0]):
-    test_row = test_X.values[i]
-    if test_row[j] == 0:
-        test_row[j] = abs(np.random.normal(sum(test_row) / len(test_row), scale=1))
+    row = test_X.values[i]
+    for j in range(0, len(row)):
+        if row[j] == 0:
+            # Considering only non-zero values in mean
+            std_mean = sum(row) / len(row[np.nonzero(row)])
+            if math.isnan(std_mean):
+                std_mean = np.random.normal(10, scale=1)
+            class_mean = sum(test_X[j]) / len(test_X[j].values[np.nonzero(test_X[j].values)])
+            row[j] = round(0.5 * (abs(np.random.normal(std_mean, scale=1))) +
+                           0.5 * (abs(np.random.normal(class_mean, scale=1))), 1)
 
 # Scatter plot each feature vs label after filling missing values
 fig = plt.figure()
@@ -56,7 +74,7 @@ for i in range(0, 2):
     for j in range(0, 3):
         counter += 1
         ax_temp = fig.add_subplot(gs[i, j])
-        ax_temp.scatter(train_full_X.get(counter - 1), train_full_Y)
+        ax_temp.scatter(train_X.get(counter - 1), train_Y)
         ax_temp.title.set_text(('Feature ' + str(counter)))
 plt.show()
 

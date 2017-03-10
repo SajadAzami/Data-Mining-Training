91f7a7b5d8ea23757d01f17e3852d97466a8eead
SajadAzami <sajjadaazami@gmail.com>
2017-03-10 12:58:43 +0330

    svr model with rbf kernel

--- DM17-AUT/HW2/Grade Prediction/main.py
+++ DM17-AUT/HW2/Grade Prediction/main.py
@@ -1,7 +1,6 @@
 """Linear Regression, 1/30/17, Sajad Azami"""
 
 import data_preparation
-import linear_regression
 import numpy as np
 import seaborn as sns
 from matplotlib import gridspec
@@ -11,7 +10,7 @@ import pandas as pd
 import math
 from sklearn.cross_validation import cross_val_score
 from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import SGDRegressor
+from sklearn.svm import SVR
 from sklearn.ensemble import GradientBoostingRegressor
 
 __author__ = 'sajjadaazami@gmail.com (Sajad Azami)'
@@ -34,17 +33,17 @@ print('Test data size:', len(test_X))
 
 # TODO use appropriate subset of features
 
-# # Scatter plot each feature vs label
-# fig = plt.figure()
-# gs = gridspec.GridSpec(2, 3)
-# counter = 0
-# for i in range(0, 2):
-#     for j in range(0, 3):
-#         counter += 1
-#         ax_temp = fig.add_subplot(gs[i, j])
-#         ax_temp.scatter(train_full_X.get(counter - 1), train_full_Y)
-#         ax_temp.title.set_text(('Feature ' + str(counter)))
-# plt.show()
+# Scatter plot each feature vs label
+fig = plt.figure()
+gs = gridspec.GridSpec(2, 3)
+counter = 0
+for i in range(0, 2):
+    for j in range(0, 3):
+        counter += 1
+        ax_temp = fig.add_subplot(gs[i, j])
+        ax_temp.scatter(train_full_X.get(counter - 1), train_full_Y)
+        ax_temp.title.set_text(('Feature ' + str(counter)))
+plt.show()
 
 # Filling missing values with Gaussian Noise, 0.5 * N(mean_of_row, 1) + 0.5 * N(mean_of_class, 1)
 for i in range(0, train_X.shape[0]):
@@ -72,119 +71,125 @@ for i in range(0, test_X.shape[0]):
             row[j] = round(0.5 * (abs(np.random.normal(std_mean, scale=1))) +
                            0.5 * (abs(np.random.normal(class_mean, scale=1))), 1)
 
-# # Scatter plot each feature vs label after filling missing values
-# fig = plt.figure()
-# gs = gridspec.GridSpec(2, 3)
-# counter = 0
-# for i in range(0, 2):
-#     for j in range(0, 3):
-#         counter += 1
-#         ax_temp = fig.add_subplot(gs[i, j])
-#         ax_temp.scatter(train_X.get(counter - 1), train_Y)
-#         ax_temp.title.set_text(('Feature ' + str(counter)))
-# plt.show()
+# Scatter plot each feature vs label after filling missing values
+fig = plt.figure()
+gs = gridspec.GridSpec(2, 3)
+counter = 0
+for i in range(0, 2):
+    for j in range(0, 3):
+        counter += 1
+        ax_temp = fig.add_subplot(gs[i, j])
+        ax_temp.scatter(train_X.get(counter - 1), train_Y)
+        ax_temp.title.set_text(('Feature ' + str(counter)))
+plt.show()
 
-# # 1. Learning Linear Regression from Data
-# print('\nLinear Regression Model:')
-# linear_regression_model = LinearRegression()
-# linear_regression_line = linear_regression_model.fit(train_X, train_Y)
-# lr_predictions = linear_regression_line.predict(test_X)
-# title = 'Linear Regression'
-# RSS = sum((test_Y - lr_predictions) ** 2)
-# print('Linear Regression\nTest RSS: ' + str(RSS))
-# cv_risk = math.sqrt(sum(abs(cross_val_score(linear_regression_model,
-#                                             train_X, train_Y, scoring='mean_squared_error', cv=10))) / 10)
-# print('10-fold CV with RMSE: ' + str(cv_risk))
-# plt.plot(np.linspace(0, 40, 40), lr_predictions, 'r', label="predictions")
-# plt.plot(np.linspace(0, 40, 40), test_Y, label="real values")
-# plt.legend(loc='lower right')
-# plt.title(title)
-# plt.show()
-#
-# # 2. Using Lasso Regression on Data
-# # Optimization Objective: (1 / (2 * n_samples)) * ||y - Xw||^2_2 + alpha * ||w||_1
-# print('\nLasso Model:')
-# alpha = 0.1
-# lasso_model = Lasso(alpha=alpha)
-# lasso_line = lasso_model.fit(train_X, train_Y)
-# title = 'Lasso with Alpha = ' + str(alpha)
-# lass_predictions = lasso_line.predict(test_X)
-# RSS = sum((test_Y - lass_predictions) ** 2)
-# print('Lasso with Lambda 0.1\nTest RSS: ' + str(RSS))
-# cv_risk = math.sqrt(sum(abs(cross_val_score(lasso_model, train_X, train_Y, scoring='mean_squared_error', cv=10))) / 10)
-# print('10-fold CV with RMSE: ' + str(cv_risk))
-# plt.plot(np.linspace(0, 40, 40), lass_predictions, 'r', label="predictions")
-# plt.plot(np.linspace(0, 40, 40), test_Y, label="real values")
-# plt.legend(loc='lower right')
-# plt.title(title)
-# plt.show()
-#
-# # Testing for different alphas
-# alphas = [0.0001, 0.001, 0.01, 0.1, 1, 10]
-# counter = 0
-# fig = plt.figure()
-# gs = gridspec.GridSpec(2, 3)
-# for i in range(0, 2):
-#     for j in range(0, 3):
-#         lasso_model = Lasso(alpha=alphas[counter])
-#         lasso_line = lasso_model.fit(train_X, train_Y)
-#         predictions = lasso_line.predict(test_X)
-#         if counter == 0:
-#             title = 'Alpha = ' + str(alphas[counter])
-#         ax_temp = fig.add_subplot(gs[i, j])
-#         ax_temp.plot(np.linspace(0, 40, 40), predictions, 'r', label="predictions")
-#         ax_temp.plot(np.linspace(0, 40, 40), test_Y, label="real values")
-#         ax_temp.legend(loc='lower right')
-#         ax_temp.title.set_text(title)
-#         counter += 1
-# plt.show()
-#
-# # 3. Learning Gradient Boosting from Data
-# print('\nGradient Boosting Model:')
-# gradient_boosting_model = GradientBoostingRegressor()
-# gradient_boosting_line = gradient_boosting_model.fit(train_X, train_Y)
-# gb_predictions = gradient_boosting_line.predict(test_X)
-# title = 'Gradient Boosting'
-# RSS = sum((test_Y - gb_predictions) ** 2)
-# print('Gradient Boosting\nTest RSS: ' + str(RSS))
-# cv_risk = math.sqrt(sum(abs(cross_val_score(gradient_boosting_model,
-#                                             train_X, train_Y, scoring='mean_squared_error', cv=10))) / 10)
-# print('10-fold CV with RMSE: ' + str(cv_risk))
-# plt.plot(np.linspace(0, 40, 40), gb_predictions, 'r', label="predictions")
-# plt.plot(np.linspace(0, 40, 40), test_Y, label="real values")
-# plt.legend(loc='lower right')
-# plt.title(title)
-# plt.show()
-
-
-# 4. Learning * Model from Data
-print('\nStochastic Gradient Descent Model:')
-sg_model = SGDRegressor()
-sg_line = sg_model.fit(train_X, train_Y)
-sg_predictions = sg_model.predict(test_X)
-title = 'Stochastic Gradient Descent'
-RSS = sum((test_Y - sg_predictions) ** 2)
-print('Stochastic Gradient Descent\nTest RSS: ' + str(RSS))
-cv_risk = math.sqrt(sum(abs(cross_val_score(sg_model,
+# 1. Learning Linear Regression from Data
+print('\nLinear Regression Model:')
+linear_regression_model = LinearRegression()
+linear_regression_line = linear_regression_model.fit(train_X, train_Y)
+lr_predictions = linear_regression_line.predict(test_X)
+title = 'Linear Regression'
+RSS = sum((test_Y - lr_predictions) ** 2)
+print('Linear Regression\nTest RSS: ' + str(RSS))
+cv_risk = math.sqrt(sum(abs(cross_val_score(linear_regression_model,
                                             train_X, train_Y, scoring='mean_squared_error', cv=10))) / 10)
 print('10-fold CV with RMSE: ' + str(cv_risk))
-plt.plot(np.linspace(0, 40, 40), sg_predictions, 'r', label="predictions")
+plt.plot(np.linspace(0, 40, 40), lr_predictions, 'r', label="predictions")
 plt.plot(np.linspace(0, 40, 40), test_Y, label="real values")
 plt.legend(loc='lower right')
 plt.title(title)
 plt.show()
 
+# 2. Using Lasso Regression on Data
+# Optimization Objective: (1 / (2 * n_samples)) * ||y - Xw||^2_2 + alpha * ||w||_1
+print('\nLasso Model:')
+alpha = 0.1
+lasso_model = Lasso(alpha=alpha)
+lasso_line = lasso_model.fit(train_X, train_Y)
+title = 'Lasso with Alpha = ' + str(alpha)
+lasso_predictions = lasso_line.predict(test_X)
+RSS = sum((test_Y - lasso_predictions) ** 2)
+print('Lasso with Lambda 0.1\nTest RSS: ' + str(RSS))
+cv_risk = math.sqrt(sum(abs(cross_val_score(lasso_model, train_X, train_Y, scoring='mean_squared_error', cv=10))) / 10)
+print('10-fold CV with RMSE: ' + str(cv_risk))
+plt.plot(np.linspace(0, 40, 40), lasso_predictions, 'r', label="predictions")
+plt.plot(np.linspace(0, 40, 40), test_Y, label="real values")
+plt.legend(loc='lower right')
+plt.title(title)
+plt.show()
 
-#
-# # Predicting the final submission file with the best model
-# # Filling missing values with Gaussian Noise, N(mean_of_row, 1)
-# final_data = pd.read_csv('../data_set/Dataset2_Unlabeled.csv', header=None)
-# for i in range(0, final_data.shape[0]):
-#     row = final_data.values[i]
-#     for j in range(0, len(row)):
-#         if row[j] == 0:
-#             row[j] = abs(np.random.normal(sum(row) / len(row), scale=1))
-# lasso_model = Lasso(alpha=0.0001)
-# lasso_line = lasso_model.fit(train_data_1, train_label_1)
-# predictions = lasso_line.predict(final_data)
-# predictions.tofile('predictions.csv', sep=',\n')
+# Testing for different alphas
+alphas = [0.0001, 0.001, 0.01, 0.1, 1, 10]
+counter = 0
+fig = plt.figure()
+gs = gridspec.GridSpec(2, 3)
+for i in range(0, 2):
+    for j in range(0, 3):
+        lasso_model = Lasso(alpha=alphas[counter])
+        lasso_line = lasso_model.fit(train_X, train_Y)
+        predictions = lasso_line.predict(test_X)
+        if counter == 0:
+            title = 'Alpha = ' + str(alphas[counter])
+        ax_temp = fig.add_subplot(gs[i, j])
+        ax_temp.plot(np.linspace(0, 40, 40), predictions, 'r', label="predictions")
+        ax_temp.plot(np.linspace(0, 40, 40), test_Y, label="real values")
+        ax_temp.legend(loc='lower right')
+        ax_temp.title.set_text(title)
+        counter += 1
+plt.show()
+
+# 3. Learning Gradient Boosting from Data
+print('\nGradient Boosting Model:')
+gradient_boosting_model = GradientBoostingRegressor()
+gradient_boosting_line = gradient_boosting_model.fit(train_X, train_Y)
+gb_predictions = gradient_boosting_line.predict(test_X)
+title = 'Gradient Boosting'
+RSS = sum((test_Y - gb_predictions) ** 2)
+print('Gradient Boosting\nTest RSS: ' + str(RSS))
+cv_risk = math.sqrt(sum(abs(cross_val_score(gradient_boosting_model,
+                                            train_X, train_Y, scoring='mean_squared_error', cv=10))) / 10)
+print('10-fold CV with RMSE: ' + str(cv_risk))
+plt.plot(np.linspace(0, 40, 40), gb_predictions, 'r', label="predictions")
+plt.plot(np.linspace(0, 40, 40), test_Y, label="real values")
+plt.legend(loc='lower right')
+plt.title(title)
+plt.show()
+
+# 4. Learning SVR Model with RBF kernel from Data
+print('\nSupport Vector Regression:')
+svr_rbf = SVR(kernel='linear', C=1e3, gamma=0.1)
+rbf_model = svr_rbf.fit(train_X, train_Y)
+rbf_predictions = rbf_model.predict(test_X)
+title = 'Support Vector Regression with RBF kernel'
+RSS = sum((test_Y - rbf_predictions) ** 2)
+print('Support Vector Regression with RBF kernel\nTest RSS: ' + str(RSS))
+cv_risk = math.sqrt(sum(abs(cross_val_score(rbf_model,
+                                            train_X, train_Y, scoring='mean_squared_error', cv=10))) / 10)
+print('10-fold CV with RMSE: ' + str(cv_risk))
+plt.plot(np.linspace(0, 40, 40), rbf_predictions, 'r', label="predictions")
+plt.plot(np.linspace(0, 40, 40), test_Y, label="real values")
+plt.legend(loc='lower right')
+plt.title(title)
+plt.show()
+
+# Predicting the final submission file with the best model, Lasso
+# Filling missing values with Gaussian Noise, N(mean_of_row, 1)
+final_X = pd.read_csv('./data_set/test.csv', header=None)
+# Filling missing values with Gaussian Noise, 0.5 * N(mean_of_row, 1) + 0.5 * N(mean_of_class, 1)
+for i in range(0, final_X.shape[0]):
+    row = final_X.values[i]
+    for j in range(0, len(row)):
+        if row[j] == 0:
+            # Considering only non-zero values in mean
+            std_mean = sum(row) / len(row[np.nonzero(row)])
+            if math.isnan(std_mean):
+                std_mean = np.random.normal(10, scale=1)
+            class_mean = sum(final_X[j]) / len(final_X[j].values[np.nonzero(final_X[j].values)])
+            row[j] = round(0.5 * (abs(np.random.normal(std_mean, scale=1))) +
+                           0.5 * (abs(np.random.normal(class_mean, scale=1))), 1)
+alpha = 0.1
+lasso_model = Lasso(alpha=alpha)
+lasso_line = lasso_model.fit(pd.concat([train_X, test_X]), pd.concat([train_Y, test_Y]))
+lasso_predictions = np.round(lasso_line.predict(final_X), 2)
+submission = pd.concat([final_X, pd.DataFrame(lasso_predictions)], axis=1)
+submission.to_csv('predictions.csv')

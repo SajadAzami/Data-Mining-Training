61542279741ab452e3475d940b0527615bfc6b0e
SajadAzami <sajjadaazami@gmail.com>
2017-03-12 13:24:41 +0330

    house prediction model training done

--- DM17-AUT/HW2/Kaggle House Prediction/main.py
+++ DM17-AUT/HW2/Kaggle House Prediction/main.py
@@ -32,7 +32,8 @@ def cat_imputation(column, value, data):
 
 train_full_X, train_full_Y = data_preparation.read_data('./data_set/train.csv', 'SalePrice')
 test_full_X = pd.read_csv('./data_set/test.csv')
-print('Data set Loaded!, Size: ' + str(train_full_X.shape))
+print('Data set Loaded!\nTrain Shape: ' + str(train_full_X.shape))
+print('Final Test Shape: ' + str(test_full_X.shape))
 
 # print('\nMissing Status:')
 # print(data_preparation.show_missing(train_full_X))
@@ -82,6 +83,8 @@ train_full_X = train_full_X.fillna(train_full_X.mean())
 test_full_X = test_full_X.fillna(test_full_X.mean())
 
 # Converting categorical to numeric
+# TODO Merge git(because of report), push till now
+# TODO Fix this
 cols_to_transform = train_full_X.columns.difference(train_full_X._get_numeric_data().columns)
 train_full_X = pd.get_dummies(train_full_X, columns=cols_to_transform)
 test_full_X = pd.get_dummies(test_full_X, columns=cols_to_transform)
@@ -94,27 +97,55 @@ test_Y = train_full_Y[1060:]
 
 print('Train data size:', train_X.shape)
 print('Test data size:', test_X.shape)
+print(test_full_X.shape)
+print(train_X.shape)
+# TODO Feature selection
 
-# # 1. Random Forest model
-# forest = RandomForestClassifier(max_features='sqrt')
-# parameter_grid = {
-#     'max_depth': [4, 5, 6, 7, 8],
-#     'n_estimators': [200, 210, 240, 250],
-#     'criterion': ['gini', 'entropy']
-# }
-#
-# cross_validation = StratifiedKFold(train_Y, n_folds=5)
-# grid_search = GridSearchCV(forest,
-#                            param_grid=parameter_grid,
-#                            cv=cross_validation)
-# grid_search.fit(train_X, train_Y)
-# print('Best score: {}'.format(grid_search.best_score_))
-# print('Best parameters: {}'.format(grid_search.best_params_))
-# Y_pred = grid_search.predict(test_full_X).astype(int)
-# submission = pd.DataFrame(Y_pred)
-# submission.to_csv(path_or_buf='random_forest.csv', index=False)
+# 1. Learning Linear Regression from Data
+print('\nLinear Regression Model:')
+linear_regression_model = LinearRegression()
+linear_regression_line = linear_regression_model.fit(train_X, train_Y)
+lr_predictions = linear_regression_line.predict(test_X)
+title = 'Linear Regression'
+RSS = sum((test_Y - lr_predictions) ** 2)
+print('Linear Regression\nTest RSS: ' + str(RSS))
+cv_risk = math.sqrt(sum(abs(cross_val_score(linear_regression_model,
+                                            train_X, train_Y, scoring='mean_squared_error', cv=10))) / 10)
+print('10-fold CV with RMSE: ' + str(cv_risk))
+n = len(lr_predictions)
+plt.plot(np.linspace(0, n, n), lr_predictions, 'r', label="predictions")
+plt.plot(np.linspace(0, n, n), test_Y, label="real values")
+plt.legend(loc='lower right')
+plt.title(title)
+plt.show()
 
-# 2. Using Lasso Regression on Data
+# 2. Random Forest model
+print('\nRandom Forrest Model:')
+forest = RandomForestClassifier(max_features='sqrt')
+parameter_grid = {
+    'max_depth': [40],
+    'n_estimators': [250],
+    'criterion': ['entropy']
+}
+
+cross_validation = StratifiedKFold(train_Y, n_folds=10)
+grid_search = GridSearchCV(forest,
+                           param_grid=parameter_grid,
+                           cv=cross_validation)
+grid_search.fit(train_X, train_Y)
+print('Best score: {}'.format(grid_search.best_score_))
+print('Best parameters: {}'.format(grid_search.best_params_))
+Y_pred = grid_search.predict(test_X).astype(int)
+title = 'Random Forrest Model'
+plt.plot(np.linspace(0, 400, 400), Y_pred, 'r', label="predictions")
+plt.plot(np.linspace(0, 400, 400), test_Y, label="real values")
+RSS = sum((test_Y - Y_pred) ** 2)
+print('Random Forrest\nTest RSS: ' + str(RSS))
+plt.legend(loc='lower right')
+plt.title(title)
+plt.show()
+
+# 3. Using Lasso Regression on Data
 # Optimization Objective: (1 / (2 * n_samples)) * ||y - Xw||^2_2 + alpha * ||w||_1
 print('\nLasso Model:')
 alpha = 0.1
@@ -132,3 +163,8 @@ plt.plot(np.linspace(0, n, n), test_Y, label="real values")
 plt.legend(loc='lower right')
 plt.title(title)
 plt.show()
+
+# Predicting the final submission file with the best model, Random Forrest
+test_prediction = grid_search.predict(test_full_X).astype(int)
+submission = pd.DataFrame([test_prediction['Id'], test_prediction], columns=['Id', 'SalePrice'])
+submission.to_csv('predictions.csv', index=None)
